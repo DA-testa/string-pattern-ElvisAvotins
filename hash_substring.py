480aa0ecacb0b5e15d17c5b6e152958f676e5a5e
ElvisAvotins <124890311+ElvisAvotins@users.noreply.github.com>
2023-04-15 19:54:32 +0000

    Completed

--- hash_substring.py
+++ hash_substring.py
@@ -1,5 +1,6 @@
 def read_input():
     file_or_input = input().strip().lower()
+    
     if file_or_input == "i":
         pattern = input().rstrip()
         text = input().rstrip()
@@ -16,28 +17,28 @@ def print_occurrences(output):
     print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
-    d = 256
-    q = 101
-    M = len(pattern)
-    N = len(text)
-    h = pow(d, M-1) % q
-    p = 0
-    t = 0
+    base = 256
+    prime = 101
+    pattern_length = len(pattern)
+    text_length = len(text)
+    h = pow(base, pattern_length - 1) % prime
+    pattern_hash = 0
+    text_hash = 0
     occurrences = []
-    for i in range(M):
-        p = (d * p + ord(pattern[i])) % q
-        t = (d * t + ord(text[i])) % q
-    for i in range(N-M + 1):
-        if p == t:
-            for j in range(M):
+    for i in range(pattern_length):
+        pattern_hash = (base * pattern_hash + ord(pattern[i])) % prime
+        text_hash = (base * text_hash + ord(text[i])) % prime
+    for i in range(text_length - pattern_length + 1):
+        if pattern_hash == text_hash:
+            for j in range(pattern_length):
                 if text[i + j] != pattern[j]:
                     break
             else:
                 occurrences.append(i)
-        if i < N-M:
-            t = (d*(t-ord(text[i])*h) + ord(text[i + M])) % q
-            if t < 0:
-                t = t + q   
+        if i < text_length - pattern_length:
+            text_hash = (base * (text_hash - ord(text[i]) * h) + ord(text[i + pattern_length])) % prime
+            if text_hash < 0:
+                text_hash += prime   
     return occurrences
 
 if __name__ == '__main__':
