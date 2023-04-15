0ab2170ce589687bef08ab972e0d20fdef370f19
ElvisAvotins <124890311+ElvisAvotins@users.noreply.github.com>
2023-04-15 18:49:03 +0000

    commit

--- hash_substring.py
+++ hash_substring.py
@@ -1,32 +1,48 @@
-# python3
-
 def read_input():
-    # this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
-    
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    file_or_input = input().strip().lower()
+    if file_or_input == "i":
+        pattern = input().rstrip()
+        text = input().rstrip()
+    elif file_or_input == "f":
+        file_path = ("tests/06")
+        with open(file_path, encoding="utf8") as f:
+            pattern = f.readline().strip()
+            text = f.readline().strip()
+    else:
+        quit()
+    return pattern, text
 
 def print_occurrences(output):
-    # this function should control output, it doesn't need any return
     print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
-    # this function should find the occurances using Rabin Karp alghoritm 
+    d = 256
+    q = 101
+    M = len(pattern)
+    N = len(text)
+    h = pow(d, M-1) % q
+    p = 0
+    t = 0
+    occurrences = []
+ 
+    for i in range(M):
+        p = (d * p + ord(pattern[i])) % q
+        t = (d * t + ord(text[i])) % q
+ 
 
-    # and return an iterable variable
-    return [0]
+    for i in range(N-M + 1):
+        if p == t:
+            for j in range(M):
+                if text[i + j] != pattern[j]:
+                    break
+            else:
+                occurrences.append(i)
+        if i < N-M:
+            t = (d*(t-ord(text[i])*h) + ord(text[i + M])) % q
+            if t < 0:
+                t = t + q
+    
+    return occurrences
 
-
-# this part launches the functions
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
-
